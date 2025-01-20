9ef93604a36df489ec3e2c3669cced16d9b5e7a1
Shaked Govrin <shakedgo@gmail.com>
2025-01-20 21:22:31 +0200

    Started Heat-logger

---
+++ main.js
@@ -0,0 +1,39 @@
+// I want to make an app to calculate the time i need to turn on the water heater
+// it should consider the average temperature of the day.
+// it should consider the time i turn on the water heater and the time i take a shower
+// it should consider the time the water heater takes to heat the water
+// it should consider the time the water heater takes to cool down
+
+let day1 = {
+    averageTemperature: 25,
+    dawnTime: '5 PM',
+    showerTime: '6 PM',
+    showerDuration: 25,
+    timeOn: 40,
+    staisfaction: 'good',
+};
+let day2 = {
+    averageTemperature: 20,
+    dawnTime: '5 PM',
+    showerTime: '6 PM',
+    showerDuration: 35,
+    timeOn: 60,
+    staisfaction: 'good',
+};
+let day3 = {
+    averageTemperature: 15,
+    dawnTime: '5 PM',
+    showerTime: '6 PM',
+    showerDuration: 35,
+    timeOn: 60,
+    staisfaction: 'not enough',
+};
+
+let day4 = {
+    averageTemperature: 20,
+    dawnTime: '5 PM',
+    showerTime: '6 PM',
+    showerDuration: 35,
+    timeOn: 80,
+    staisfaction: 'too much',
+};
